53675fbd2144253827fd159f7371399f73b0baba
Varun <varunr.paliwal@gmail.com>
2025-09-05 23:04:29 +0530

    feat: add turborepo

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,13 +1,12 @@
+import { add } from "@workspace/math/add";
 import { Button } from "@workspace/ui/components/button";
-import { Input } from "@workspace/ui/components/input";
-import { add } from "../../../packages/math/src/add";
+
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World/web</h1>
+        <h1 className="text-2xl font-bold">Hello World/widget</h1>
         <Button size="sm">Button</Button>
-        <Input />
         <p>The sum of 2 and 3 is: {add(2, 3)}</p>
       </div>
     </div>
