55898fd6f35c0d0cca252524240d5cfbf1d86bf0
Varun <varunr.paliwal@gmail.com>
2025-09-06 20:13:03 +0530

    02: added convex as internal package in shadcn/ui

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,16 @@
+"use client";
+import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
-import { Input } from "@workspace/ui/components/input";
-import { add } from "../../../packages/math/src/add";
+import { useMutation, useQuery } from "convex/react";
 export default function Page() {
+  const users = useQuery(api.user.getMay);
+  const addUser = useMutation(api.user.addUser);
+
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World/web</h1>
-        <Button size="sm">Button</Button>
-        <Input />
-        <p>The sum of 2 and 3 is: {add(2, 3)}</p>
-      </div>
+      <Button onClick={() => addUser()}> ADD </Button>
+      <p>app/web</p>
+      {JSON.stringify(users)}
     </div>
   );
 }
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,14 +1,16 @@
-import { add } from "@workspace/math/add";
+"use client";
+import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
-
+import { useMutation, useQuery } from "convex/react";
 export default function Page() {
+  const users = useQuery(api.user.getMay);
+  const addUser = useMutation(api.user.addUser);
+  console.log(users);
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World/widget</h1>
-        <Button size="sm">Button</Button>
-        <p>The sum of 2 and 3 is: {add(2, 3)}</p>
-      </div>
+      <Button onClick={() => addUser()}> ADD </Button>
+      <p>app/widget</p>
+      {JSON.stringify(users)}
     </div>
   );
 }
