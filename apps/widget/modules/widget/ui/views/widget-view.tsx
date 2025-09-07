c27b8317152782284b8e60dffbca9872b8de3293
Varun <varunr.paliwal@gmail.com>
2025-09-08 01:29:13 +0530

    11: Added custom widget screen route JOTAI

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-// import { WidgetFooter } from "../components/widget-footer";
+import { useAtomValue } from "jotai";
+import { screenAtom } from "../../atoms/widget-atoms";
 import { WidgetAuthScreen } from "../screens/widget-auth-screen";
 
 interface Props {
@@ -8,10 +9,20 @@ interface Props {
 }
 
 export const WidgetView = ({ organizationId }: Props) => {
+  const screen = useAtomValue(screenAtom);
+  const screenComponents = {
+    error: <p>TODO: ERROR</p>,
+    loading: <p>TODO: LOADING</p>,
+    chat: <p>TODO: CHAT</p>,
+    selection: <p>TODO: SELECTION</p>,
+    voice: <p>TODO: VOICE</p>,
+    auth: <WidgetAuthScreen />,
+    inbox: <p>TODO: INBOX</p>,
+    contact: <p>TODO: CONTACT</p>,
+  };
   return (
     <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-      <WidgetAuthScreen />
-      {/* <WidgetFooter /> */}
+      {screenComponents[screen]}
     </main>
   );
 };
